9b770e2e4a9030084f3894222d355d2d5c90b603
Jimi Som <johnjimy.som@student.csulb.edu>
2015-09-14 17:34:30 +0900

    Create StudentTester.cpp

---
+++ lab3/StudentTester.cpp
@@ -0,0 +1,18 @@
+    #include "Date.cpp"
+    #include <iostream>
+    #include <cstring> 
+    #include <string>
+    #include <cstdlib>  
+    #include <ctime> 
+    #include <iomanip>                 
+using namespace std;
+
+struct student
+{
+   string name;
+   int id;
+   int grade;
+   Date birthday;
+};
+
+//unfinished but for a reason
